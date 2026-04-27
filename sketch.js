88196a0186fd34921ec3292c8494c1b298796634
s110476-cmd <s110476@htsh.ntpc.edu.tw>
2026-04-27 11:05:39 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -56,21 +56,24 @@ function draw() {
         }
 
         // Define the segments to connect with lines
+        // 0-4: 大拇指與手腕, 5-8: 食指, 9-12: 中指, 13-16: 無名指, 17-20: 小指
         let segments = [
-          [0, 1, 2, 3, 4],    // Thumb & Wrist
-          [5, 6, 7, 8],       // Index Finger
-          [9, 10, 11, 12],    // Middle Finger
-          [13, 14, 15, 16],   // Ring Finger
-          [17, 18, 19, 20]    // Pinky
+          [0, 1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16],
+          [17, 18, 19, 20]
         ];
 
         // Draw lines for each segment
         strokeWeight(4);
+        noFill();
         for (let segment of segments) {
           for (let i = 0; i < segment.length - 1; i++) {
             let pt1 = hand.keypoints[segment[i]];
             let pt2 = hand.keypoints[segment[i + 1]];
 
+            // 將影像座標映射到畫布上的置中縮放位置
             let x1 = map(pt1.x, 0, video.width, x, x + displayW);
             let y1 = map(pt1.y, 0, video.height, y, y + displayH);
             let x2 = map(pt2.x, 0, video.width, x, x + displayW);
